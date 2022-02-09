da4806be97b9c1aa808aab92703d1ae3e79526e9
Kenneth Berland <ken@hero.net>
2022-02-09 12:51:44 -0800

    use config.yml from the docker image

--- scripts/localbuild.sh
+++ scripts/localbuild.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-IMAGE=595508394202.dkr.ecr.us-west-2.amazonaws.com/syn-bungee-servers
-TAG=local
-
 if [[ -z $1 ]]
 then
     echo "doing the build"
     rm -rf tmp
     ./scripts/localbuild.rb | grep -v 'aws ecr' | bash
     rm -f plugins.tar
-    docker build -t $IMAGE:$TAG -f scripts/Dockerfile.local .
-
 else
     echo "not building. just running"
 fi
